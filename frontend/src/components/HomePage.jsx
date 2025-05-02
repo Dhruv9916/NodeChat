ff810507da9781d2e04f26cfdd361eb17b63a198
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-05-02 18:35:03 +0530

    Frontend Routing is done

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      I a a Home Page
+    </div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+       I am a Login Page
+    </div>
+  )
+}
+
+export default Login
