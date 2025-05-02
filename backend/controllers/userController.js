7b9bda7afc590f2f8c7fad9960e17984664b5687
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-05-02 14:57:46 +0530

    Login Logic is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const register = async (req,res) => {
+//Register Logic
+export const register = async (req, res) => {
   try {
     const { fullname, username, password, confirmPassword, gender } = req.body;
     if (!fullname || !username || !password || !confirmPassword || !gender) {
@@ -40,6 +42,59 @@ export const register = async (req,res) => {
       message: "Account created successfully",
       success: true,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Login Logic
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        _id: user._id,
+        username: user.username,
+        fullname: user.fullname,
+        profilePhoto: user.profilePhoto,
+      });
   } catch (error) {
     console.log(error);
   }
