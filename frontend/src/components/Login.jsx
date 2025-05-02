307616c081f4b17a3c81a36815d1485d2396f4e4
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-05-02 19:42:44 +0530

    Sign up and Login Page form handling done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
   return (
-    <div>
-       I am a Login Page
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-3xl font-bold text-center">Login</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="text"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <p className="text-center my-2">
+            Don't have an account? <Link to="/signup"> signup </Link>
+          </p>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
