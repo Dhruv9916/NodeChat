343a359db716717a09deb39514878626a9809b59
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-05-05 20:26:26 +0530

    Get Other Users Custom hook is made

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-function OtherUser() {
+function OtherUser(props) {
+  const user = props.user;
   return (
     <div>
       <div className="flex gap-2 items-center hover:bg-zinc-200 rounded-sm p-2 cursor-pointer">
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://marketplace.canva.com/EAF21qlw744/1/0/1600w/canva-blue-modern-facebook-profile-picture-mtu4sNVuKIU.jpg"
-              alt="user-profile"
-            />
+            <img src={user?.profilePhoto} alt="user-profile" />
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex justify-between  gap-2 ">
-            <p> Mernstack</p>
+            <p> {user?.fullname}</p>
           </div>
         </div>
       </div>
--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import OtherUser from "./OtherUser";
+import useGetOtherUser from "../hooks/useGetOtherUser";
+import { useSelector } from "react-redux";
 
 const OtherUsers = () => {
+  //my custom hooks
+  useGetOtherUser();
+  const { otherUsers } = useSelector((store) => store.user);
+  if (!otherUsers) {
+    //early return in react
+    return;
+  }
+
   return (
     <div className="overflow-auto">
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
-      <OtherUser />
+      {otherUsers?.map((user) => {
+        return <OtherUser key={user._id} user={user} />;
+      })}
     </div>
   );
 };
